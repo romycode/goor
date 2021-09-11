42ed6fbf9f700aed21ec31b0bd458636d2811da4
Albert Romera Mulero <albert.romera.arm@gmail.com>
2021-09-11 13:19:33 +0200

    feat: add tag selector

---
+++ pkg/selector/tag.go
@@ -0,0 +1,67 @@
+package selector
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type TagSelector struct {
+	tag string
+}
+
+func (t TagSelector) Match(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.Data == t.tag
+}
+
+type TagParser struct {
+	SelParser
+}
+
+func NewTagParser(sel string) *TagParser {
+	return &TagParser{
+		SelParser{
+			sel:    sel,
+			selLen: len(sel),
+		},
+	}
+}
+
+func (t *TagParser) Parse() (Sel, error) {
+	if !(t.isValidTagNameChar(t.sel[t.pos]) || t.sel[t.pos] == '\\') {
+		return nil, fmt.Errorf("expected tag selector (key), found '%c'", t.sel[t.pos])
+	}
+
+	tag := ""
+	for t.pos < t.selLen {
+		char := t.sel[t.pos]
+
+		switch {
+		case t.isValidTagNameChar(char): // get current "i" if is a valid name character
+			tag += string(char)
+			t.pos++
+			break
+		case char == '\\': // sel have an escaped element https://drafts.csswg.org/css-syntax-3/#escaping
+			c, err := t.parseEscape()
+			if err != nil {
+				return nil, err
+			}
+			tag += c
+			break
+		case char == '\r':
+			t.pos++
+			if t.sel[t.pos] == '\n' {
+				t.pos++ // for end of lines \r\n
+			}
+			break
+		case char == ' ' || char == '\n' || char == '\t':
+			t.pos++
+			break
+		}
+	}
+
+	return &TagSelector{
+		tag: strings.ToLower(tag),
+	}, nil
+}
---
+++ pkg/selector/tag_test.go
@@ -0,0 +1,95 @@
+package selector
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTagParser_Parse(t1 *testing.T) {
+	tests := []struct {
+		name     string
+		selector string
+		want     Sel
+	}{
+		{
+			name:     "parse a basic tag",
+			selector: "section",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse a basic tag with \\n",
+			selector: "sect\nion",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse a basic tag with \\r",
+			selector: "sect\rion",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse a basic tag with \\t",
+			selector: "sect\tion",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse a basic tag with \\r\\n",
+			selector: "sect\r\nion",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse a basic tag with whitespace",
+			selector: "sect ion",
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with escaped element with 6 digits '\\000073' (s)",
+			selector: `\000073ection`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with multiple escaped element with 6 digits '\\000073' (s) and '\\000069' (i)",
+			selector: `\000073ect\000069on`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with escaped element with 2 digits and whitespace '\\73' (s)",
+			selector: `\73 ection`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with multiple escaped element with 2 digits and whitespace '\\73' (s) and '\\69' (i)",
+			selector: `\73 ection`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with escaped UNICODE element '\\U+000073' (s)",
+			selector: `\U+000073ection`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with multiple escaped UNICODE element '\\U+000073' (s) and '\\U+000069' (i)",
+			selector: `\U+000073ect\U+000069on`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with escaped UNICODE element with 4 digits and whitespace '\\U+0073' (s)",
+			selector: `\U+0073 ection`,
+			want:     &TagSelector{tag: "section"},
+		},
+		{
+			name:     "parse tag with multiple escaped UNICODE element with 4 digits and whitespace '\\U+0073' (s) and '\\U+0069' (i)",
+			selector: `\U+0073 ect\U+0069 on`,
+			want:     &TagSelector{tag: "section"},
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(
+			tt.name, func(t1 *testing.T) {
+				t := NewTagParser(tt.selector)
+				if got, _ := t.Parse(); !reflect.DeepEqual(got, tt.want) {
+					t1.Errorf("Parse() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
